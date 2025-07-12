c262589dd3c86a265daf93edd6398f31075f3030
Saberan <v.kotelnikovdidi@gmail.com>
2025-07-12 19:42:59 +0300

    Refactor Place model by adding latitude, longitude, and description fields; enhance rating field with validators.

--- backend/places/models.py
+++ backend/places/models.py
@@ -1,4 +1,6 @@
+from decimal import Decimal
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 # Create your models here.
@@ -6,8 +8,18 @@ class Place(models.Model):
     name = (models.CharField(max_length=200),)
     address = models.CharField(max_length=300)
     delivery = models.BooleanField(default=False)
+    latitude = models.DecimalField(
+        max_digits=9, decimal_places=6, blank=True, null=True
+    )
+    longitude = models.DecimalField(
+        max_digits=9, decimal_places=6, blank=True, null=True
+    )
+    description = models.TextField(blank=True, null=True)
     rating = models.DecimalField(
-        max_digits=3, decimal_places=2, default=0.0
+        max_digits=3,
+        decimal_places=2,
+        default=Decimal(0.0),
+        validators=[MinValueValidator(0.0), MaxValueValidator(10.0)],
     )  # TODO add validators
     main_image = models.ImageField(upload_to="place_images/")
     additional_images = models.ImageField(
