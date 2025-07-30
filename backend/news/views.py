e0408fe285f70e0fb596e40e745d68e236db4c88
Saberan <v.kotelnikovdidi@gmail.com>
2025-07-30 21:11:25 +0300

    feat: add authentication permission to NewsViewSet

--- backend/news/views.py
+++ backend/news/views.py
@@ -1,10 +1,8 @@
 import logging
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from rest_framework.decorators import action
-
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from news.models import News
-from news.serializers import NewsSerializer
+from .serializers import NewsSerializer
 
 logger = logging.getLogger("news")
 
@@ -14,3 +12,4 @@ logger = logging.getLogger("news")
 class NewsViewSet(viewsets.ViewSet):
     queryset = News.objects.filter(is_published=True)
     serializer_class = NewsSerializer
+    permission_classes = [IsAuthenticated]
