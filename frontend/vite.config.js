4f7688a88e128cd7417d1258f50e8655d6f2c6e4
Saberan <v.kotelnikovdidi@gmail.com>
2025-08-08 21:14:19 +0300

    rework tailwind

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,10 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [svelte()],
-  server: {
-    host: true,
-    port: 5173,
-  },
-});
+  plugins: [
+    svelte(),
+    tailwindcss(),
+  ],
+})
